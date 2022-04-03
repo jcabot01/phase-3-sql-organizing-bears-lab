e74b253f1c0c4feed66c3c2419e9c5b9504a196e
JonCabot <jfc0053@gmail.com>
2022-04-03 08:11:16 -0500

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 2, "M", "Black", "Mean", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 5, "M", "Brown", "Crazy", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 7, "F", "Blonde", "Sweet", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 10, "M", "Brown", "Calm", 1);
+INSERT INTO bears VALUES (5, "Melissa", 1, "F", "Red", "Silly", 1);
+INSERT INTO bears VALUES (6, "Grinch", 15, "M", "Green", "Mean", 0);
+INSERT INTO bears VALUES (7, "Wendy", 6, "F", "Black", "Nice", 1);
+INSERT INTO bears VALUES (NULL, NULL, NULL, NULL, NULL, NULL, NULL);
